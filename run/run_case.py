708bce5864873d43b2dfc8842e6761a23cb5b3a9
万园浩 <63204593@qq.com>
2021-01-21 11:44:04 +0800

    '指定文件push'

--- run/run_case.py
+++ run/run_case.py
@@ -21,7 +21,6 @@ if __name__ == '__main__':
     report_path = open(f"./test_report/{time}result.html", 'wb')
     runner = HTMLTestRunner.HTMLTestRunner(stream=report_path, title=u"自动化测试报告(详情请看附件)", description="执行结果",verbosity=2)
     ss = runner.run(run_test_case())
-    # 关闭文件，如果不关闭文件后面的程序读取，会是空内容
     report_path.close()
 
     # 打开测试报告
